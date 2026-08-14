c7193c041c51b84ebabc37cf886001531dd3d61b
Fausiyat <fausiyatmahmood@gmail.com>
2026-08-14 14:28:06 +0100

    update

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -907,6 +907,9 @@ export default function AdminDashboard() {
                   <option value="paint_rubber">Paint Rubber</option>
                   <option value="mudu">Mudu / Module</option>
                   <option value="basket">Full Basket</option>
+                  <option value="10kg">10kg</option>
+                  <option value="5kg">5kg</option>
+                  <option value="2.5kg">2.5kg</option>
                   <option value="dozen">One Dozen (12 pcs)</option>
                   <option value="carton">Carton</option>
                   <option value="crate">Crate</option>
